2554bf07a840ba96e063a716e31cfa9ee3a50ad3
Pavel Blinov <pblinov@crystalit.net>
2014-05-08 00:51:27 +0600

    disable complaint global context_processor

--- _step_9/_project_v9_/settings.py
+++ _step_9/_project_v9_/settings.py
@@ -61,7 +61,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "complaints.context_processors.complaints",
+    # "complaints.context_processors.complaints",
 )
 
 ROOT_URLCONF = '_project_v9_.urls'
