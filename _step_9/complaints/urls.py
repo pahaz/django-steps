3934bfff555efe5a327bb8b607717787f7459fd2
Pavel Blinov <pblinov@crystalit.net>
2014-05-08 01:17:53 +0600

    finish step_9

--- _step_9/complaints/urls.py
+++ _step_9/complaints/urls.py
@@ -1,3 +1,5 @@
+from complaints.views import ComplaintIndexView, ComplaintDetailView
+
 __author__ = 'pahaz'
 
 from django.conf.urls import patterns, include, url
@@ -8,5 +10,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', "complaints.views.index", name="complaints_index"),
+   # url(r'^$', "complaints.views.index", name="complaints_index"),
+   url(r'^$', ComplaintIndexView.as_view(), name="complaints_index"),
+   url(r'^page/(?P<page>[0-9]+)/$', ComplaintIndexView.as_view(), name="complaints_index_page"),
+   # url(r'^(?P<pk>[0-9]+)/$', "complaints.views.detail", name="complaints_detail"),
+   url(r'^(?P<pk>[0-9]+)/$', ComplaintDetailView.as_view(), name="complaints_detail"),
 )
